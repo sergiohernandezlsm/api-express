dde54eedd2eea0c28c0f08a4cb5983e29208f946
Sergio  Hernandez <sergiohernandez@sergios-mbp.lsm.local>
2019-10-21 18:30:13 +0100

    Update record request

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const records = require('./src/functions');
 app.use(express.json());
 
 
-// Send a GET request to "/list" to READ list of record
+// Send a GET request to "/records" to READ list of record
 app.get( '/records', async (req, res) => {
   try {
     const list = await records.getRecords();
@@ -17,38 +17,51 @@ app.get( '/records', async (req, res) => {
   }
 });
 
-// Send a POST request to "/list" to CREATE record
+// Send a POST request to "/records" to CREATE record
 app.post( '/records', async (req , res) => {
   const list = await records.getRecords();
   const recordsJson = list.records;
   recordsJson.push(req.body);
   const jsonRecords = JSON.stringify({records: recordsJson}, null, 2);
+  
+  // rewrite data.json to update obj
   fs.writeFile('./data/data.json', jsonRecords, (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   });
-})
+});
 
-// Send a DELETE request to "/list/:id" to DELETE records
+// Send a DELETE request to "/records/:id" to DELETE records
 app.delete( '/records/:id', async (req, res) => {
   const list = await records.getRecords();
-  const allRecords = list.records;
-  const recordsId = allRecords.map(x => x.id);
-  const recordToDelete = req.params.id;  
-  list.records = list.records.filter(item => item.id != recordToDelete);
-  
+  const recordId = req.params.id;  
+  list.records = list.records.filter(item => item.id != recordId);
   const jsonRecords = JSON.stringify({records: list.records}, null, 2);
+
+  // rewrite data.json to update obj
   fs.writeFile('./data/data.json', jsonRecords, (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   });
-  console.log(jsonRecords)
-})
-
-// Send a PUT request to "/list/:id" to UPDATE records
+});
 
 
-
+// Send a PUT request to "/records/:id" to UPDATE records
+app.put( '/records/:id', async (req, res) => {
+  const list = await records.getRecords();
+  const recordId = req.params.id; 
+  const toChange = list.records.find( item => item.id == recordId );
+  toChange.name = req.body.name;
+  toChange.lastname = req.body.lastname;
+  const jsonRecords = JSON.stringify({records: list.records}, null, 2);
+  
+  // rewrite data.json to update obj
+  fs.writeFile('./data/data.json', jsonRecords, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
+  
+});
 
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
