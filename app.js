1b26341d2de2582c9d517f6972d2738c4eb78f0f
Sergio  Hernandez <sergiohernandez@sergios-mbp.lsm.local>
2019-10-24 08:52:10 +0100

    extend router path to include more migrations

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const routes = require('./src/routes/users');
+const routesUsers = require('./src/routes/users');
+const routesCars = require('./src/routes/cars');
+
 
 app.use(express.json());
-app.use('/api', routes);
+app.use('/api', routesUsers);
+app.use('/api', routesCars);
 
 app.listen(3000, () => console.log('users API listening on port 3000!'));
 
